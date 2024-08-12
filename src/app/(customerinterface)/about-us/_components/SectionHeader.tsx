1631b2d5b89a93e107575b3b70db458f1bcc5e8a
Aminu sulaiman yusuf <aminugotie42@gmail.com>
2024-08-12 21:34:23 +0300

    headersection

--- src/app/(customerinterface)/about-us/_components/SectionHeader.tsx
+++ src/app/(customerinterface)/about-us/_components/SectionHeader.tsx
@@ -1,6 +1,12 @@
-const SectionHeader = ({ title }:any) => (
-    <h1 className="text-3xl font-bold mb-4">{title}</h1>
-  );
-  
-  export default SectionHeader;
-  
+import React from 'react';
+
+// Define the type for the `title` prop as `string`
+interface SectionHeaderProps {
+  title: string;
+}
+
+const SectionHeader: React.FC<SectionHeaderProps> = ({ title }) => (
+  <h1 className="text-3xl font-bold mb-4">{title}</h1>
+);
+
+export default SectionHeader;
