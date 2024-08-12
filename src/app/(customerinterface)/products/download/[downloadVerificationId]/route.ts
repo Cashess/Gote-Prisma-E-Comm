c24837464e97c7ca0ec7524d75d0540250e28f3f
Aminu sulaiman yusuf <aminugotie42@gmail.com>
2024-08-12 21:55:43 +0300

    download Lenght

--- src/app/(customerinterface)/products/download/[downloadVerificationId]/route.ts
+++ src/app/(customerinterface)/products/download/[downloadVerificationId]/route.ts
@@ -1,47 +1,30 @@
-import database from "@/db";
-import { NextRequest, NextResponse } from "next/server";
-import fs from "fs/promises";
+import database from "@/db"
+import { NextRequest, NextResponse } from "next/server"
+import fs from "fs/promises"
 
-export async function GET(req: NextRequest, { params }: { params: { downloadVerificationId: string } }) {
-    const { downloadVerificationId } = params;
+export async function GET(
+  req: NextRequest,
+  {
+    params: { downloadVerificationId },
+  }: { params: { downloadVerificationId: string } }
+) {
+  const data = await database.downloadVerification.findUnique({
+    where: { id: downloadVerificationId, expiresAt: { gt: new Date() } },
+    select: { product: { select: { filePath: true, name: true } } },
+  })
 
-    try {
-        // Fetch the data from the database
-        const data = await database.downloadVerification.findUnique({
-            where: { 
-                id: downloadVerificationId, 
-                expiresAt: { gt: new Date() } 
-            },
-            select: { 
-                product: { 
-                    select: { 
-                        filePath: true, 
-                        name: true 
-                    } 
-                } 
-            }
-        });
+  if (data == null) {
+    return NextResponse.redirect(new URL("/products/download/expired", req.url))
+  }
 
-        // Redirect if data or product is not found
-        if (data == null || data.product == null) {
-            return NextResponse.redirect(new URL("/products/download/expired", req.url));
-        }
+  const { size } = await fs.stat(data.product.filePath)
+  const file = await fs.readFile(data.product.filePath)
+  const extension = data.product.filePath.split(".").pop()
 
-        // Get file information
-        const { size } = await fs.stat(data.product.filePath);
-        const file = await fs.readFile(data.product.filePath);
-        const extension = data.product.filePath.split(".").pop();
-
-        // Return the file as an attachment
-        return new NextResponse(file, {
-            headers: {
-                "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
-                "Content-Length": size.toString(),
-            }
-        });
-    } catch (error) {
-        // Handle unexpected errors
-        console.error("Error processing the request:", error);
-        return NextResponse.error();
-    }
+  return new NextResponse(file, {
+    headers: {
+      "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
+      "Content-Length": size.toString(),
+    },
+  })
 }
