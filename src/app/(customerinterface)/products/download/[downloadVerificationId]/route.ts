b4957d34d193f4b4ecc7d2f8be7f1e30cc825551
Aminu sulaiman yusuf <aminugotie42@gmail.com>
2024-08-13 20:08:53 +0300

    downloadid

--- src/app/(customerinterface)/products/download/[downloadVerificationId]/route.ts
+++ src/app/(customerinterface)/products/download/[downloadVerificationId]/route.ts
@@ -1,12 +1,11 @@
-import database from "@/db"
+import path from "path"
+import mime from "mime"
 import { NextRequest, NextResponse } from "next/server"
 import fs from "fs/promises"
 
 export async function GET(
   req: NextRequest,
-  {
-    params: { downloadVerificationId },
-  }: { params: { downloadVerificationId: string } }
+  { params: { downloadVerificationId } }: { params: { downloadVerificationId: string } }
 ) {
   const data = await database.downloadVerification.findUnique({
     where: { id: downloadVerificationId, expiresAt: { gt: new Date() } },
@@ -17,14 +16,22 @@ export async function GET(
     return NextResponse.redirect(new URL("/products/download/expired", req.url))
   }
 
-  const { size } = await fs.stat(data.product.filePath)
-  const file = await fs.readFile(data.product.filePath)
-  const extension = data.product.filePath.split(".").pop()
+  try {
+    const filePath = data.product.filePath
+    const { size } = await fs.stat(filePath)
+    const file = await fs.readFile(filePath)
+    const extension = path.extname(filePath).slice(1) // get file extension without dot
+    const contentType = mime.getType(extension) || 'application/octet-stream'
 
-  return new NextResponse(file, {
-    headers: {
-      "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
-      "Content-Length": size.toString(),
-    },
-  })
+    return new NextResponse(file, {
+      headers: {
+        "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
+        "Content-Length": size.toString(),
+        "Content-Type": contentType,
+      },
+    })
+  } catch (error) {
+    console.error('Error handling file download:', error)
+    return NextResponse.json({ error: 'Unable to process the file request' }, { status: 500 })
+  }
 }
